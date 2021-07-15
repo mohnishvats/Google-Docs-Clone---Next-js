b32a014c2c5f96b5044a5dd026b7d61a414fc846
mohnish <mohnishvats@gmail.com>
2021-07-15 16:30:23 +0530

    building header

---
+++ components/Header.js
@@ -0,0 +1,49 @@
+import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
+function Header() {
+  return (
+    <div className="sticky top-0 z-50 flex items-center px-4 py-2 shadow-md bg-white">
+      <Button
+        color="gray"
+        buttonType="outline"
+        rounded="true"
+        iconOnly="true"
+        ripple="dark"
+        className=" h-20 w-20 border-0"
+      >
+        <Icon name="menu" size="3xl" />
+      </Button>
+
+      <Icon name="description" size="5xl" color="blue" />
+
+      <h1 className=" ml-2 text-gray-700 text-2xl">Docx</h1>
+
+      <div className=" mx-5 md:mx-20 flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-600 rounded-lg focus-within:text-gray-600 focus-within:shadow-md">
+        <Icon name="search" size="3xl" color="gray" />
+        <input
+          type="text"
+          placeholder="search"
+          className="flex-grow px-5 text-base bg-transparent outline-none"
+        />
+      </div>
+
+      <Button
+        color="gray"
+        buttonType="outline"
+        rounded="true"
+        iconOnly="true"
+        ripple="dark"
+      >
+        <Icon name="apps" size="3xl" color="gray" />
+      </Button>
+
+      <img
+        loading="lazy"
+        className="cursor-pointer h-12 w-12 rounded-full ml-2"
+        src="https://lh5.googleusercontent.com/-plrDxGqAqng/AAAAAAAAAAI/AAAAAAAAAAA/xd1U0AhhFbY/s96/photo.jpg"
+      />
+    </div>
+  );
+}
+
+export default Header;
